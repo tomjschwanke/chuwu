e92e0cea3d1fc5ae292dd82c31b90c74cf5d1cc5
Tom Jason Schwanke <tomschwanke80@gmail.com>
2021-02-21 13:36:14 +0100

    register CommandExecutor and TabCompleter on startup

--- src/main/java/de/tomjschwanke/mc/chuwu/Chuwu.java
+++ src/main/java/de/tomjschwanke/mc/chuwu/Chuwu.java
@@ -3,6 +3,8 @@ package de.tomjschwanke.mc.chuwu;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Objects;
+
 public final class Chuwu extends JavaPlugin {
 
     private static Chuwu instance;
@@ -22,6 +24,11 @@ public final class Chuwu extends JavaPlugin {
         // Init database
         ChuwuPlayerData playerData = new ChuwuPlayerData();
         playerData.initDatabase();
+
+        // Register commands
+        ChuwuCommands commands = new ChuwuCommands();
+        Objects.requireNonNull(getCommand("chuwu")).setExecutor(commands);
+        Objects.requireNonNull(getCommand("chuwu")).setTabCompleter(commands);
 
         // Register chat event
         getServer().getPluginManager().registerEvents(new ChuwuEventListener(), this);
